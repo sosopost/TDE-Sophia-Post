b6f0b6eab41b909fc5e0ec6154edb1a2e9ac2a73
sosopost <sophiapost.ploposki@icloud.com>
2024-08-24 10:30:40 -0300

    TDE-Sophia Post

--- TDE.py
+++ TDE.py
@@ -5,30 +5,41 @@ with open('teste.txt', 'r') as arquivo:
         x = linha.split()
         matriz.append(x)
 
+def formatar(conjunto):
+    return ', '.join(conjunto)
+
 for i in range(len(matriz)):
-    for operacao in matriz[i]:
-        if len(matriz[i]) == 1:
+    if len(matriz[i]) == 1:
+        operacao = matriz[i][0]
+        if i + 2 < len(matriz):
+            conjunto1 = set(matriz[i + 1][0].split(','))
+            conjunto2 = set(matriz[i + 2][0].split(','))
+
             if operacao == 'U':
-                print("\nOperação:", operacao)
-                print()
-                if i + 2 < len(matriz):
-                    print(f"União: Conjunto 1 {set(matriz[i + 1])} Conjunto 2 {set(matriz[i + 2])}. \nResultado da operação: {set(matriz[i + 1]) | set(matriz[i + 2])}")
-                    print()
+                resultado = conjunto1 | conjunto2
+                print(f"\nOperação: União")
+                print(f"\nConjunto 1: {{{formatar(conjunto1)}}}")
+                print(f"Conjunto 2: {{{formatar(conjunto2)}}}")
+                print(f"\nResultado da operação: \n{{{formatar(resultado)}}}")
+
             elif operacao == 'I':
-                print("\nOperação:", operacao)
-                print()
-                if i + 2 < len(matriz):
-                    print(f"Intersecção: Conjunto 1 {set(matriz[i + 1])}, Conjunto 2 {set(matriz[i + 2])}. \nResultado da operação: {set(matriz[i + 1]) & set(matriz[i + 2])}")
-                    print()
+                resultado = conjunto1 & conjunto2
+                print(f"\nOperação: Intersecção")
+                print(f"\nConjunto 1: {{{formatar(conjunto1)}}}")
+                print(f"Conjunto 2: {{{formatar(conjunto2)}}}")
+                print(f"\nResultado da operação: \n{{{formatar(resultado)}}}")
+
             elif operacao == 'D':
-                print("\nOperação:", operacao)
-                print()
-                if i + 2 < len(matriz):
-                    print(f"Diferença: Conjunto 1 {set(matriz[i + 1])}, Conjunto 2 {set(matriz[i + 2])}. \nResultado da operação: {set(matriz[i + 1]) - set(matriz[i + 2])}")
-                    print()
+                resultado = conjunto1 - conjunto2
+                print(f"\nOperação: Diferença")
+                print(f"\nConjunto 1: {{{formatar(conjunto1)}}}")
+                print(f"Conjunto 2: {{{formatar(conjunto2)}}}")
+                print(f"\nResultado da operação: \n{{{formatar(resultado)}}}")
+
             elif operacao == 'C':
-                print("\nOperação:", operacao)
-                print()
-                if i + 2 < len(matriz):
-                    cartesiano = [(a, b) for a in set(matriz[i + 1]) for b in set(matriz[i + 2])]
-                    print(f"Produto Cartesiano: Conjunto 1 {set(matriz[i + 1])}, Conjunto 2 {set(matriz[i + 2])}. \nResultado da operacão: {cartesiano}")
+                resultado = [(a, b) for a in conjunto1 for b in conjunto2]
+                cartesiano = ', '.join([f"({a}, {b})"for a, b in resultado])
+                print(f"\nOperação: Produto Cartesiano")
+                print(f"\nConjunto 1: {{{formatar(conjunto1)}}}")
+                print(f"Conjunto 2: {{{formatar(conjunto2)}}}")
+                print(f"\nResultado da operação: \n{cartesiano}")
